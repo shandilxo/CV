52334bd8396869efb66d80d1ccd396fd269b91c0
SHANDIL THAKUR <158510180+shandilxo@users.noreply.github.com>
2024-02-10 21:40:12 +0530

    hero remove

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import Cover from '@/components/Cover';
 import CardSection from '@/components/sections/CardSection';
 import MoreProjects from '@/components/sections/MoreProjects';
-import Hero from '@/components/sections/Hero';
 import ProjectCardSection from '@/components/sections/ProjectCardSection';
 import {
   backendCard,
@@ -19,7 +18,6 @@ export default function Home() {
     <div>
       <Cover />
       <div className="flex flex-col gap-10 lg:px-10">
-        {/* <Hero /> */}
         <ProjectCardSection title="About" data={projectCard} />
         <MoreProjects title="More " data={moreProjects} />
       </div>
