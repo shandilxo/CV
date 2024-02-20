f3755e85de7e0a0c5b9fd0e784639ad54c66b070
SHANDIL THAKUR <158510180+shandilxo@users.noreply.github.com>
2024-02-20 23:29:22 +0530

    s

--- src/components/cover/ProfilePic.tsx
+++ src/components/cover/ProfilePic.tsx
@@ -8,9 +8,10 @@ const ProfilePic: FC<ProfilePicProps> = ({}) => {
         <div className="">
             <Image
                 alt="Shandil Thakur"
-                src="/images/photoh.jpg"
+                src="/images/pho.jpg"
                 height={120}
                 width={120}
+                quality={100}
                 className="object-fill border-3 border-slate-800 rounded-full shadow-md"
             />
         </div>
