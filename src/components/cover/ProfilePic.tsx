7e88976102d9ef166b6f90ea4a2a833d1dd70e1c
SHANDIL THAKUR <158510180+shandilxo@users.noreply.github.com>
2024-03-09 18:03:40 +0530

    randi changes

--- src/components/cover/ProfilePic.tsx
+++ src/components/cover/ProfilePic.tsx
@@ -1,17 +1,19 @@
 import Image from 'next/image';
 import { FC } from 'react';
+import styles from './ProfilePic.module.css'; // Import CSS file for styling
 
 interface ProfilePicProps {}
 
 const ProfilePic: FC<ProfilePicProps> = ({}) => {
     return (
-        <div className="">
+        <div className={styles.profilePicContainer}>
+            <div className={styles.animatedBorder}></div> {/* Border animation */}
             <Image
                 alt="Shandil Thakur"
                 src="/images/pho.jpg"
                 height={120}
                 width={120}
-                className="object-fill border-3 border-slate-800 rounded-full shadow-md"
+                className={`object-fill rounded-full shadow-md ${styles.animatedBorder}`}
             />
         </div>
     );
